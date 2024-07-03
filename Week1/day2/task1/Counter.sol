79989e138310fbd320758c5aa4768bffb2b38aa6
day0xy <jasonfan1337@gmail.com>
2024-07-03 11:39:49 +0800

    add Counter.sol

---
+++ Week1/day2/task1/Counter.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+// 定义一个合约
+contract Counter {
+    uint public counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function count() public {
+        counter = counter + 1;
+    }
+
+    function get() public view returns (uint) {
+        return counter;
+    }
+
+    function add(uint x) public {
+        counter = x + counter;
+    }
+}
