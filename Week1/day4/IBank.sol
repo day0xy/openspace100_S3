df076604570e0b6758e202ef981f9d6cb87a8517
day0xy <jasonfan1337@gmail.com>
2024-07-05 16:10:45 +0800

    add IBank.sol

--- Week1/day4/IBank.sol
+++ Week1/day4/IBank.sol
@@ -2,5 +2,18 @@
 pragma solidity >=0.8.0;
 
 interface IBank {
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
 
+    //public变量的getter函数
+    function admin() external view returns (address);
+
+    //public变量的getter函数
+    function topDepositors(uint256 index) external view returns (address);
+
+    function deposit() external payable;
+
+    function withdraw(uint256 amount) external;
+
+    function getDeposit(address user) external view returns (uint256);
 }
