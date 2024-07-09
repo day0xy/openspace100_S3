e1eb28d08588dec7419c7a40bd59962bd845d02a
day0xy <jasonfan1337@gmail.com>
2024-07-09 23:08:28 +0800

    finish ERC20WithHook

--- Week2/day6/task1/ERC20WithHook.sol
+++ Week2/day6/task1/ERC20WithHook.sol
@@ -1,2 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+interface TokenRecipient {
+    function tokensReceived(address from, uint256 amount)
+        external
+        returns (bool);
+}
+
+contract MyToken is ERC20 {
+    using Address for address;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function transferWithCallback(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        _transfer(_msgSender(), recipient, amount);
+
+        if (recipient.code.length > 0) {
+            bool rv = TokenRecipient(recipient).tokensReceived(
+                _msgSender(),
+                amount
+            );
+            require(rv, "No tokensReceived");
+        }
+
+        return true;
+    }
+}
