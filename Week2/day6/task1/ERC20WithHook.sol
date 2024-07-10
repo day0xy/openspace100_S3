9c2af005b8aff76e04f7b0e799b0830d595f7d5a
day0xy <jasonfan1337@gmail.com>
2024-07-10 18:48:40 +0800

    1

--- Week2/day6/task1/ERC20WithHook.sol
+++ Week2/day6/task1/ERC20WithHook.sol
@@ -2,27 +2,55 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-
-interface TokenRecipient {
-    function tokensReceived(address from, uint256 amount) external returns (bool);
-}
+import "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
 
 contract MyToken is ERC20 {
-    using Address for address;
+    constructor() ERC20("day0xy", "day0xy_token") {}
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    // transfer failed
+    error ERC1363TransferFailed(address to, uint256 value);
 
-    function transferWithCallback(address recipient, uint256 amount) external returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
+    error ERC1363ReceiveFailed(address to);
 
-        //判断如果是合约
-        if (recipient.code.length > 0) {
-            bool rv = TokenRecipient(recipient).tokensReceived(_msgSender(), amount);
-            require(rv, "No tokensReceived");
+    function transferAndcall(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) public returns (bool) {
+        if (!transfer(to, value)) {
+            revert ERC1363TransferFailed(to, value);
         }
 
+        _checkOnTransferReceived(msg.sender, to, value, data);
+
         return true;
+    }
+
+    // hooks
+    function _checkOnTransferReceived(
+        address from,
+        address to,
+        uint256 value,
+        bytes memory data
+    ) private {
+        if (to.code.length > 0) {
+            return;
+        }
+        // invoke hook
+        try
+            IERC1363Receiver(to).onTransferReceived(
+                msg.sender,
+                from,
+                value,
+                data
+            )
+        returns (bytes4 retval) {
+            if (retval != IERC1363Receiver.onTransferReceived.selector) {
+                revert ERC1363ReceiveFailed(to);
+            }
+        } catch (bytes memory reason) {
+            revert ERC1363ReceiveFailed(to);
+        }
     }
 }
