7dec87fba4c3f8d343260aad7bdf05e535430f42
day0xy <jasonfan1337@gmail.com>
2024-07-16 15:54:32 +0800

    add W3D1- walletconnect-demo

---
+++ Week3/W3D1/src/config/index.tsx
@@ -0,0 +1,34 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { holesky, mainnet, polygon, sepolia } from "wagmi/chains";
+
+// Get projectId from https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+if (!projectId) throw new Error("Project ID is not defined");
+
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia, holesky, polygon] as const;
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  auth: {
+    email: true, // default to true
+    socials: ["google", "x", "github", "discord", "apple"],
+    showWallets: true, // default to true
+    walletFeatures: true, // default to true
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+});
