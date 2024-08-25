61db8f8bc253fdb1e65db4f38ba132192e5fd648
day0xy <jasonfan1337@gmail.com>
2024-08-25 15:51:30 +0800

    1

--- Week3/W3D5/thegraph/src/nft-market.ts
+++ Week3/W3D5/thegraph/src/nft-market.ts
@@ -4,47 +4,47 @@ import {
   EIP712DomainChanged as EIP712DomainChangedEvent,
   List as ListEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
-  SetWhiteListSigner as SetWhiteListSignerEvent
-} from "../generated/NFTMarket/NFTMarket"
+  SetWhiteListSigner as SetWhiteListSignerEvent,
+} from "../generated/NFTMarket/NFTMarket";
 import {
   BuyNFT,
   Delist,
   EIP712DomainChanged,
   List,
   OwnershipTransferred,
-  SetWhiteListSigner
-} from "../generated/schema"
+  SetWhiteListSigner,
+} from "../generated/schema";
 
 export function handleBuyNFT(event: BuyNFTEvent): void {
   let entity = new BuyNFT(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.buyer = event.params.buyer
-  entity.price = event.params.price
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+  );
+  entity.seller = event.params.seller;
+  entity.buyer = event.params.buyer;
+  entity.price = event.params.price;
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDelist(event: DelistEvent): void {
   let entity = new Delist(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+  );
+  entity.seller = event.params.seller;
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEIP712DomainChanged(
@@ -52,31 +52,31 @@ export function handleEIP712DomainChanged(
 ): void {
   let entity = new EIP712DomainChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleList(event: ListEvent): void {
   let entity = new List(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.seller = event.params.seller
-  entity.payToken = event.params.payToken
-  entity.price = event.params.price
-  entity.deadline = event.params.deadline
+  );
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.seller = event.params.seller;
+  entity.payToken = event.params.payToken;
+  entity.price = event.params.price;
+  entity.deadline = event.params.deadline;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -84,26 +84,26 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetWhiteListSigner(event: SetWhiteListSignerEvent): void {
   let entity = new SetWhiteListSigner(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.signer = event.params.signer
+  );
+  entity.signer = event.params.signer;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
